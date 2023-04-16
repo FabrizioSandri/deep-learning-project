5cc73ba707d4313149bde8d424e924c17a07b060
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2023-04-16 21:02:13 +0200

    module execution main and documentaiton

--- src/dataset.py
+++ src/dataset.py
@@ -2,7 +2,6 @@ import os
 import pickle
 import re
 import pandas as pd
-import numpy as np
 
 from torchvision.io import read_image
 from torch.utils.data import Dataset, DataLoader
@@ -18,6 +17,21 @@ class RefCOCOg(Dataset):
         # load the annotations
         self.loadAnnotations()
 
+    def __len__(self):
+        return len(self.annotations.shape[0])
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.annotations.loc[idx, "file_name"])
+        
+        image = read_image(img_path)
+        sentences = [ sentence["sent"] for sentence in self.annotations.loc[idx, "sentences"] ]
+       
+        return image, sentences
+
+    '''
+    Auxiliary function used to load the annotations and the location of the 
+    images from an annotations file.
+    '''
     def loadAnnotations(self): 
         try:
             f = open(self.annotations_file, 'rb')
@@ -33,17 +47,10 @@ class RefCOCOg(Dataset):
 
         self.annotations = df
 
-    def __len__(self):
-        return len(self.annotations.shape[0])
-
-    def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.annotations.loc[idx, "file_name"])
-        
-        image = read_image(img_path)
-        sentences = [ sentence["sent"] for sentence in self.annotations.loc[idx, "sentences"] ]
-       
-        return image, sentences
-
+    '''
+    Given an index specified by the idx parameter, this function plots the
+    corresponding image along with the sentences associated to it.
+    '''
     def plot_img(self, idx):
         image, sentences = self.__getitem__(idx)
 
@@ -51,3 +58,10 @@ class RefCOCOg(Dataset):
         plt.imshow(image.permute(1,2,0))
        
         plt.show()
+
+
+if __name__ == '__main__':
+
+    dataloader = RefCOCOg("/home/fabri/Downloads/refcocog/annotations/refs(umd).p", "/home/fabri/Downloads/refcocog/images")
+    img,sen = dataloader.__getitem__(0)
+    dataloader.plot_img(0)
