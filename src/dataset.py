bb233fc4944b7467f76a6184e3295558f230ff97
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2023-04-17 17:24:45 +0200

    dataset partitioning

--- src/dataset.py
+++ src/dataset.py
@@ -10,33 +10,46 @@ import numpy as np
 
 class RefCOCOg(Dataset):
 
-    def __init__(self, annotations_file, img_dir):
-        self.annotations_file = annotations_file
-        self.img_dir = img_dir
+    '''
+    Constructor for the RefCOCOg class
 
-        # load the annotations
-        self.loadAnnotations()
+    Args:
+        - annotations_file: path to the file containing the annotations
+        - img_dir: path to the directory containing the images
+        - test_set: when set to True this class will load the test part of the
+            dataset
+    '''
+    def __init__(self, annotations_file, img_dir, test_set):
+        self.img_dir = img_dir
+        self.test_set = test_set
+
+        # load the dataset with the annotations
+        self.loadDataset(annotations_file)
 
     def __len__(self):
-        return self.annotations.shape[0]
+        return self.dataset.shape[0]
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.annotations.loc[idx, "file_name"])
+        img_path = os.path.join(self.img_dir, self.dataset.loc[idx, "file_name"])
         
         image = read_image(img_path)
-        sentences = [ sentence["sent"] for sentence in self.annotations.loc[idx, "sentences"] ]
+        sentences = self.dataset.loc[idx, "sentences"]
        
         return image, sentences
 
     '''
-    Auxiliary function used to load the annotations and the location of the 
-    images from an annotations file.
+    Load the dataset with the annotations in the form of sentences along with
+    the location of the images. This function is supposed to combine the
+    sentences of the same image into a single sample.
+
+    Args:
+        - annotations_file: path to the file containing the annotations
     '''
-    def loadAnnotations(self): 
+    def loadDataset(self, annotations_file): 
         try:
-            f = open(self.annotations_file, 'rb')
+            f = open(annotations_file, 'rb')
         except OSError:
-            print(f"Could not open/read file: " % self.annotations_file)
+            print(f"Could not open/read file: " % annotations_file)
             sys.exit()
 
         # parse the pickle file
@@ -45,24 +58,37 @@ class RefCOCOg(Dataset):
         # Fix: remove from the file name the last digit after the last underscore
         df["file_name"] = df["file_name"].apply(lambda file_name: re.sub("_[0-9]+.jpg", ".jpg", file_name))
 
-        self.annotations = df
+        if self.test_set:
+            df = df[df["split"] == "test"]
+        else:
+            df = df[df["split"] == "train"]
+
+        # group together the same images into a single sample
+        df = df.groupby('image_id').agg({'file_name': 'first', 'sentences': 'sum'}).reset_index()
+        df["sentences"] = df["sentences"].apply(lambda sentences: [s["sent"] for s in sentences])
+        self.dataset = df
+
 
     '''
-    Given an index specified by the idx parameter, this function plots the
-    corresponding image along with the sentences associated to it.
+    Plot an image along with the sentences associated to it.
+
+    Args:
+        - idx: index of the image to plot
     '''
     def plot_img(self, idx):
         image, sentences = self.__getitem__(idx)
 
         plt.title("\n".join(sentences))
         plt.imshow(image.permute(1,2,0))
-       
+        plt.tight_layout()
+        
         plt.show()
 
 
 if __name__ == '__main__':
 
-    dataloader = RefCOCOg("/home/fabri/Downloads/refcocog/annotations/refs(umd).p", "/home/fabri/Downloads/refcocog/images")
+    train_dataset = RefCOCOg("/home/fabri/Downloads/refcocog/annotations/refs(umd).p", "/home/fabri/Downloads/refcocog/images", test_set=False)
+    test_dataset = RefCOCOg("/home/fabri/Downloads/refcocog/annotations/refs(umd).p", "/home/fabri/Downloads/refcocog/images", test_set=True)
 
-    random_idx = np.random.randint(0,len(dataloader))
-    dataloader.plot_img(random_idx)
+    random_idx = np.random.randint(0, len(train_dataset))
+    train_dataset.plot_img(random_idx)
