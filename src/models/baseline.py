ae2275af9409ecb466d880b5386ab60bc2b56161
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2023-07-13 19:54:26 +0200

    baseline with yolo

--- src/models/baseline.py
+++ src/models/baseline.py
@@ -1,12 +1,14 @@
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
 import torch
+import torchvision
+import matplotlib
 import clip
+import numpy as np
+import matplotlib.pyplot as plt
 
 from torchvision import transforms
+from torchvision.ops import box_convert
 
-class YoloBaseline():
+class Baseline():
 
   def __init__(self, device=None):
     if device is None:
@@ -23,12 +25,14 @@ class YoloBaseline():
     matplotlib.use(current_backend)
 
   '''
-  Runs inference and returns a bounding box in the format 'xyxy'. Note that if 
-  no matching bounding box is found by this method, the function returns None.
+  Runs inference and returns a bounding box in the format 'xyxy' and the
+  predicted class for the object. Note that if no matching bounding box is found
+  by this method, the function returns a bounding box that covers the whole 
+  image.
 
   Args:
       - image: a torch tensor defining the image
-      - description: a textual description 
+      - description: a textual description
       - plot: if set to true plots all the bounding boxes with their similarity
   '''
   def inference(self, image, description, plot=False):
@@ -41,9 +45,9 @@ class YoloBaseline():
     yolo_results = self.yoloModel(image).pandas()
     cropped_images = self.crop_bbox(yolo_results, image)
 
-    # Stop here if YOLO doesn't find any bounding box
+    # Stop here if YOLO doesn't find any bounding box and return the bounding box covering the whole image
     if len(cropped_images) == 0:
-      return None
+      return torch.tensor([0, 0, 640, 640], device=self.device), 0
 
     #################### CLIP ####################
     preprocessed_images = self.preprocess_images(cropped_images)
@@ -52,9 +56,11 @@ class YoloBaseline():
     # latent representations of the cropped images and the textual description
     with torch.no_grad():
       image_features = self.clipModel.encode_image(preprocessed_images)
+      image_features /= image_features.norm(dim=-1, keepdim=True)
       text_features = self.clipModel.encode_text(tokenized_description).float()
-    
-    # compute the cosine similarity between each pair of images and the textual 
+      text_features /= text_features.norm(dim=-1, keepdim=True)
+
+    # compute the cosine similarity between each pair of images and the textual
     # descriptions
     similarities = []
     for obj_i in range(image_features.shape[0]):
@@ -73,12 +79,13 @@ class YoloBaseline():
     xmax = res.loc[best_bbox,"xmax"]
     ymax = res.loc[best_bbox,"ymax"]
     pred_bbox = torch.tensor([xmin, ymin, xmax, ymax], device=self.device)
+    pred_class = test_dataset.name2category[res.loc[best_bbox,"name"]]
 
-    return pred_bbox
+    return pred_bbox, pred_class
 
   '''
   This method plots all the bounding boxes found by this Baseline with the
-  corresponding value of similarity 
+  corresponding value of similarity
   '''
   def plot_results(self, similarities, cropped_images):
     nrows = int(np.ceil(len(similarities)/4))
@@ -97,7 +104,7 @@ class YoloBaseline():
   '''
   Given as input the results returned by yolo by running inference on 'image',
   this method returns a list of images as a result of cropping them according
-  to the bounding boxes detected by YOLO.
+  to the bounding boxes detected by YOLO
 
   Args:
     - yolo_results: pandas dataframe returned by Yolo after running inference
@@ -106,7 +113,7 @@ class YoloBaseline():
   def crop_bbox(self, yolo_results, image):
     num_boxes = len(yolo_results.xyxy[0])
     cropped_images = []
-    
+
     res = yolo_results.xyxy[0]
     for i in range(num_boxes):
       xmin = res.loc[i,"xmin"]
@@ -114,7 +121,7 @@ class YoloBaseline():
       xmax = res.loc[i,"xmax"]
       ymax = res.loc[i,"ymax"]
       bbox = torch.tensor([xmin, ymin, xmax, ymax], device=self.device)
-      bbox = box_convert(bbox, 'xyxy', 'xywh')            
+      bbox = box_convert(bbox, 'xyxy', 'xywh')
 
       cropped = transforms.functional.crop(image, int(bbox[1].item()), int(bbox[0].item()), int(bbox[3].item()), int(bbox[2].item()))
       cropped_images.append(cropped)
@@ -122,10 +129,10 @@ class YoloBaseline():
     return cropped_images
 
   '''
-  Preprocess a list of cropped images, by stacking all of them into a single 
+  Preprocess a list of cropped images, by stacking all of them into a single
   tensor. For example if this method takes as input 5 images of different sizes,
   then the result is a tensor of size [5,3,224,224] i.e. the first dimensions is
-  for the number of images, the second one for the number of color planes(3 in 
+  for the number of images, the second one for the number of color planes(3 in
   RGB) and the last twos describe the width and height of each image.
 
   Args:
@@ -144,8 +151,8 @@ class YoloBaseline():
 
 
   '''
-  This method computes the cosine similarity between the latent representation 
-  of the text and the latent representation of an image. 
+  This method computes the cosine similarity between the latent representation
+  of the text and the latent representation of an image.
   '''
   def cosine_similarity(self, image_encoding, text_encoding):
     cos = torch.nn.CosineSimilarity()
